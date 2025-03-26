2f542bf7b9ea68e635de4ec1dcda480e1a28ec30
Ary1810 <ary.alberto1810@gmail.com>
2025-03-26 09:16:05 -0700

    Add files via upload

---
+++ Practica2.py
@@ -0,0 +1,57 @@
+from flask import Flask
+from flask import Flask, json
+
+app = Flask(__name__)
+
+@app.route('/wellcome/')
+
+@app.route('/wellcome/<name>')
+
+@app.route('/wellcome/<int:ncontrol>')
+
+@app.route('/wellcome/<name>/<int:ncontrol>')
+
+def bienvenido(name=None,ncontrol=None):
+
+    if name== None and ncontrol==None:
+
+        return 'Bienvenido '
+
+    if name!= None and ncontrol == None:
+
+        return f'Bienvenido {name}'
+
+    if name == None and ncontrol != None:
+
+        return f'El número recibido es: {ncontrol}'
+
+    else:
+
+        return f'Bienvenido {name} tu numero de control es: {ncontrol}'
+
+
+a=8
+
+print(a)
+
+print(type(a))
+
+a='mexicali'
+
+print(a)
+
+print(type(a))
+
+a=4.5
+
+print(a)
+
+print(type(a))
+
+a=None
+
+print(a)
+
+print(type(a))
+
+ 
